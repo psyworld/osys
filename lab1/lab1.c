cfca2f108161aad3f5b9de753a7e7b69cf51b3f1
psyworld <psygalaxia@gmail.com>
2017-09-24 02:25:41 +0300

    Update lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -26,31 +26,31 @@ int main(int argc, char ** argv)
 			//creating 10Mb file
 			int X = 1024 * 1024 * 10;
 			FILE* fp = fopen("myfile", "w");
-    		fseek(fp, X , SEEK_SET);
-    		fputc('\0', fp);
-    		fclose(fp);
+    			fseek(fp, X , SEEK_SET);
+    			fputc('\0', fp);
+    			fclose(fp);
 
-    		printf("I'm just a child process, a pathetic copy\n");
-    		printf("My pid = %d \n", getpid());
+    			printf("I'm just a child process, a pathetic copy\n");
+    			printf("My pid = %d \n", getpid());
 
-    		exit(0);
-    	}
+    			exit(0);
+    		}
 
-    	default: //parent
-    	{ 
-    		printf("I'm a parent process!\n");
-    		printf("My pid = %d \n", getppid());
+    		default: //parent
+    		{ 
+    			printf("I'm a parent process!\n");
+    			printf("My pid = %d \n", getppid());
 
-    		int status = 0;
+    			int status = 0;
 
-    		clock_t begin = clock();
-    		waitpid(pid, &status, 0);
-    		clock_t end   = clock(); 
+    			clock_t begin = clock();
+    			waitpid(pid, &status, 0);
+    			clock_t end   = clock(); 
 
-    		printf("Parent process waited child process %f seconds \n", 
+    			printf("Parent process waited child process %f seconds \n", 
     			    (double)(end - begin) / CLOCKS_PER_SEC);
 
-    	}
+    		}
 	}	
 }
 
