71ed62a60454f6200611d447cfc66c17346577c2
mila <psyworld>
2017-09-30 23:59:19 +0300

    random elements

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -23,34 +23,40 @@ int main(int argc, char ** argv)
 		case 0: //child
 		{
 
-			//creating 10Mb file
+		    //creating 10Mb file
 			int X = 1024 * 1024 * 10;
-			FILE* fp = fopen("myfile", "w");
-    		fseek(fp, X , SEEK_SET);
-    		fputc('\0', fp);
-    		fclose(fp);
 
-    		printf("I'm just a child process, a pathetic copy\n");
-    		printf("My pid = %d \n", getpid());
+            FILE* fp = fopen("myfle", "w");
+
+            fprintf(fp, "I'm just a child process, a pathetic copy\n My pid = %d \n I'm a parent process! \n  My ppid: %d\n", getpid(), getppid());
+                 
+            srand(time(NULL));
+
+            char byte;
+            for(int i = 0; i < X; i++){
+                byte = rand();
+                fwrite(&byte, sizeof(char), 1, fp);
+            }
+
+            fclose(fp);
 
     		exit(0);
     	}
 
-    	default: //parent
-    	{ 
-    		printf("I'm a parent process!\n");
-    		printf("My pid = %d \n", getppid());
-
-    		int status = 0;
-
-    		clock_t begin = clock();
-    		waitpid(pid, &status, 0);
-    		clock_t end   = clock(); 
-
-    		printf("Parent process waited child process %f seconds \n", 
+    		default: //parent
+    		{ 
+    			//printf("I'm a parent process!\n");
+    			//printf("My pid = %d \n", getppid());
+	
+    			int status = 0;
+	
+    			clock_t begin = clock();
+    			waitpid(pid, &status, 0);
+    			clock_t end   = clock(); 
+	
+    			printf("Parent process waited child process %f seconds \n", 
     			    (double)(end - begin) / CLOCKS_PER_SEC);
-
-    	}
+	    	}
 	}	
 }
 
